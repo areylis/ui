f5e79643cb9b2a228eb352053ac49cf95c8a2f9f
areylis <areylistal@gmail.com>
2023-12-19 21:44:41 +0300

    add submodule

--- src/components/inputs/Button/Button.tsx
+++ src/components/inputs/Button/Button.tsx
@@ -1,5 +1,8 @@
 import {Button} from 'react-aria-components';
+import {diff} from "../../../../submodule/utils/src/utils/array/diff.mjs";
 
 export function MyButton() {
+   const test = diff([2,1,4], [1,4,5,6])
+   console.log(test)
    return <Button>test</Button>
 }
